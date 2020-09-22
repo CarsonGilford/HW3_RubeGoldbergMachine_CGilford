3fd29f317976d30fce216da8f37724de276ac6f0
Carson <ceg488@nyu.edu>
2020-09-22 12:23:46 -0400

    First commit. Complete HW3 of Rube Goldberg Machine. Ready to tern in!

---
+++ Assets/code/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public Transform cameraTransform; 
+   Transform cameraFocusTarget;
+   //Auto happen when something with a rigidBody2D enters Trigger
+void OnTriggerEnter2D(Collider2D activator){
+   Debug.Log(activator.name +"Something eneter this trigger");
+
+   //Move camera
+   cameraFocusTarget = activator.transform;
+}
+
+void Update(){
+   if (cameraFocusTarget != null){
+   cameraTransform.position = cameraFocusTarget.transform.position + new Vector3(0f,0f, -10f);
+  }
+}
+
+void OnTriggerExit2D(Collider2D activator){
+   if (activator.transform == cameraFocusTarget){
+      cameraFocusTarget = null;
+      
+  }
+}
+}
---
+++ Assets/code/ending.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ending : MonoBehaviour
+{
+  void OnTriggerEnter2D(Collider2D activator){
+   Debug.Log(activator.name +"Something eneter this trigger");
+   //Release balls
+   Destroy(gameObject);
+  }
+}
